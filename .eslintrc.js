5a0d8fb6f8ace04f1df04cceea8a5c168b5c08fb
Liucong <2621454384@qq.com>
2019-07-01 11:34:18 +0800

    add HomeSwiper

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
+    'eol-last': 0,
+    //这句话表示在文件末尾可以不加回车，贴个链接  https://eslint.org/docs/rules/eol-last
+    'space-before-function-paren': 0,
+    //这句话表示在函数后可以不加空格
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-image"
+                                                                   :src="item.imgUrl"
+                                                                   alt=""></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/p/tts6/1703/a1/bf7e61143113d02.jpg_r_390x260x90_3f15ccec.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://img1.qunarzz.com/p/tts0/1703/75/6d627693f77b1b02.jpg_r_640x420x90_fbad911b.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+
+  .wrapper
+    width: 100%;
+    height: 31.25vw
+    background-color: #eee
+
+    .swiper-image
+      width: 100%;
+      height: 31.25vw
+</style>
