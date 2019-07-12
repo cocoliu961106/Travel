1e29dcfec70f75c52282ad734f6b25fb2c7acd43
Liucong <2621454384@qq.com>
2019-07-12 15:33:03 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="header">
-      <div class="header-left">
-        <div class="iconfont back-icon">&#xe653;</div>
-      </div>
-      <div class="header-input">
-        <span class="iconfont">&#xe65b;</span>
-        输入城市/景点/游玩主题</div>
-      <div class="header-right">
-        {{this.city}}
-        <span class="iconfont arrow-icon">&#xe61d;</span></div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe653;</div>
     </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe65b;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <router-link to="/city">
+      <div class="header-right">
+          北京
+        <span class="iconfont arrow-icon">&#xe61d;</span>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -28,12 +32,15 @@ export default {
     line-height: .86rem
     background: $bgColor
     color: #fff
+
     .header-left
       width: .64rem
       float: left
+
       .back-icon
         text-align: center
         font-size: .4rem
+
     .header-input
       flex: 1
       height: .64rem
@@ -44,12 +51,14 @@ export default {
       background: #fff
       border-radius: .1rem
       color: #ccc
+
     .header-right
       min-width: 1.04rem
       padding: 0 .1rem
       float: right
       text-align: center
       color: #fff
+
       .arrow-icon
         font-size: .24rem
         margin-left: -.04rem
